a291b7f6b70641598dd87ef6cb76b2348b6659a5
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-06-27 01:44:49 -0400

    added to user model

--- models/user.js
+++ models/user.js
@@ -8,17 +8,27 @@ const userSchema = new Schema(
       type: String,
       unique: true,
       required: true,
-      trim: true
+      trim: true,
+      minlength: 4,
+      maxlength: 20
     },
-    assignments: [assignmentSchema],
-  },
-  {
-    toJSON: {
-      getters: true,
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ // Regular expression for email validation
     },
-  }
-);
-
+    thoughts: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  });
+  
 const User = model('User', userSchema);
 
 module.exports = User;
