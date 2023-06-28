e4c92f3aab5b99aa50f915805736a7e8dd5d5387
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-06-28 14:35:35 -0400

    api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const thoughtsRoutes = require('./thoughtsRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/thoughts', thoughtsRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
